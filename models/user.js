3e8f87e1a3565f266b0bcba54e2d51d309bf8a71
Vuduc123 <vuhoaiduc123@gmail.com>
2021-03-30 13:04:32 +0700

    30/03/2021

--- models/user.js
+++ models/user.js
@@ -10,13 +10,28 @@ class User {
     const db = getDb();
     return db.collection('users').insertOne(this);
   }
+
+  
   static mongoFindByID (id) {
     const db = getDb();
-    db.collection('users').findOne({_id:id})
+    return db.collection('users').findOne({_id:id})
     .then(result => {
       return result
     })
     .catch(err => console.log(err))
   }
+
+
+  static mongoFindOneUser(value) {
+    const db = getDb();
+     return db.collection('users')
+      .findOne({email:value})
+      .then(result => {
+        return result
+      })
+      .catch(err => console.log(err));
+  }
 }
+
+ 
 module.exports = User;
